36ae342f7d299da1e5fcfb4c37bc29939e241c0c
hfjxjjd123 <licbusiness21@gmail.com>
2023-06-30 14:54:58 +0900

    누락된 것 추가

--- src/bin/a18b.rs
+++ src/bin/a18b.rs
@@ -22,4 +22,64 @@
 // * Print whether the employee may access the building
 //   * Must use a function that utilizes the question mark operator to do this
 
-fn main() {}
+fn main() {
+let employees: Vec<Employee> = vec![
+    Employee::new(EmployeeType::Manager, true, "Hakrim Lee"),
+    Employee::new(EmployeeType::AssemblyTechnician, true, "Songess"),
+    Employee::new(EmployeeType::KitchenStaff, false, "juki"),
+    Employee::new(EmployeeType::Manager, false, "Cho gu seong"),
+    Employee::new(EmployeeType::MaintenanceCrew, true, "Jaeho Yang"),
+    Employee::new(EmployeeType::LineSupervisor, true, "Hakrim Min"),
+];
+
+for employee in employees{
+    let validate = validation(employee);
+    match validate{
+        Err(e) => println!("{:?}", e),
+        _ => {}
+    }
+}
+
+}
+
+enum EmployeeType{
+    MaintenanceCrew,
+    MarketingDepartment,
+    Manager,
+    LineSupervisor,
+    KitchenStaff,
+    AssemblyTechnician
+}
+
+struct Employee{
+    types: EmployeeType,
+    current: bool,
+    name: String,
+}
+impl Employee{
+    fn new(types: EmployeeType, current: bool, name: &str) -> Self{
+        Self{
+            types: types,
+            current: current,
+            name: name.to_owned()
+        }
+    }
+}
+
+fn valid_enter(employee: Employee) -> Result<Employee,String> {
+    if !employee.current {
+        Err("NOT ALLOWED - expired".to_owned())
+    } else{
+        match employee.types {
+            EmployeeType::MaintenanceCrew | EmployeeType::MarketingDepartment | EmployeeType::Manager 
+                => Ok(employee),
+            _ => Err("NOT ALLOWED - authority".to_owned()) 
+        }
+    }
+}
+
+fn validation(employee: Employee) -> Result<(),String> {
+    let enter_employee: Employee = valid_enter(employee)?;
+    println!("{:?} can pass", enter_employee.name);
+    Ok(())
+}
