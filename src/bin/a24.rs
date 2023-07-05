8f71b1e1399b5807d90714a3441f7226ca784fbc
hfjxjjd123 <licbusiness21@gmail.com>
2023-07-05 16:15:25 +0900

    iterator operation

--- src/bin/a24.rs
+++ src/bin/a24.rs
@@ -10,4 +10,15 @@
 
 fn main() {
     let data = vec![1, 2, 3, 4, 5];
+    
+    let mut changed_data = vec![];
+    changed_data = data
+    .iter()
+    .map(|num| num*3 )
+    .filter(|num| num > &10)
+    .collect();
+    
+    for datus in changed_data{
+        println!("{:?}", datus);
+    }
 }
