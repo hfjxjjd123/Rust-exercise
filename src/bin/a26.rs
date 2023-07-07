9adae9f13a5808bd4ea9513d5b85c5fd9db07904
hfjxjjd123 <licbusiness21@gmail.com>
2023-07-07 12:35:49 +0900

    7/7 외부 Crate 사용(시간반환)

--- src/bin/a26.rs
+++ src/bin/a26.rs
@@ -8,4 +8,16 @@
 // * (OPTIONAL) Read the documentation section `Formatting and Parsing`
 //   for examples on how to create custom time formats
 
-fn main() {}
+use chrono::Utc;
+use chrono::TimeZone;
+use std::convert::TryInto;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+fn main() {
+    let time_now = SystemTime::now();
+    let duration = time_now.duration_since(UNIX_EPOCH).expect("Failed to get duration");
+    let seconds_since_epoch = duration.as_secs();
+    let formatted_datetime = Utc.timestamp_opt(seconds_since_epoch.try_into().unwrap(), 0).unwrap();
+
+    println!("{:?}", formatted_datetime.to_rfc2822());
+}
